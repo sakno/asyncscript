021ebeb3dc0748c5f0ca4405d70c8d1ee4b075cc
sakno <sakno@tncor.com>
2011-10-07 01:44:22 +1100

    AST node for complex expression

---
+++ csharp/src/interpreter/Compiler/Ast/ScriptCodeComplexExpression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DynamicScript.Compiler.Ast
+{
+    using ComVisibleAttribute = System.Runtime.InteropServices.ComVisibleAttribute;
+
+    [ComVisible(false)]
+    [Serializable]
+    public sealed class ScriptCodeComplexExpression: ScriptCodeExpression
+    {
+        public readonly ScriptCodeStatementCollection Body;
+
+        private ScriptCodeComplexExpression(ScriptCodeStatementCollection body)
+        {
+            Body = body ?? new ScriptCodeStatementCollection();
+        }
+
+        public ScriptCodeComplexExpression(params ScriptCodeStatement[] statements)
+            : this(new ScriptCodeStatementCollection(statements))
+        {
+        }
+
+        internal override bool Completed
+        {
+            get { return true; }
+        }
+
+        public override bool Equals(ScriptCodeExpression other)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Expression Restore()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Verify()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override ScriptCodeExpression Visit(ISyntaxTreeNode parent, Converter<ISyntaxTreeNode, ISyntaxTreeNode> visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ScriptCodeExpression Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
