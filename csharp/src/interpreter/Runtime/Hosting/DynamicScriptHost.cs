a6b2298ab88f20230d702223e8aa01edba52fa7c
sakno <sakno@tncor.com>
2011-10-10 22:10:34 +1100

    A branch before merge

--- csharp/src/interpreter/Runtime/Hosting/DynamicScriptHost.cs
+++ csharp/src/interpreter/Runtime/Hosting/DynamicScriptHost.cs
@@ -21,10 +21,6 @@ namespace DynamicScript.Runtime.Hosting
         [LoaderOptimization(LoaderOptimization.SingleDomain)]
         private static int Main(string[] args)
         {
-            var r = DynamicScriptInterpreter.Run(@"
-            var s = {const s = gets(); leave s;};
-            puts(s);
-            ");
             return Execute(new CommandLineParser(Console.Out, Console.In), args);
         }
     }
