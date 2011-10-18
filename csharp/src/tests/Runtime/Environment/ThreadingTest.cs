bbcf4de8d1d2959bf57d064e6f68e63003881d0d
sakno <sakno@tncor.com>
2011-10-18 19:39:26 +1100

    Threading tests

---
+++ csharp/src/tests/Runtime/Environment/ThreadingTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using DynamicScript.Testing;
+
+namespace DynamicScript.Runtime.Environment
+{
+    [TestClass(typeof(ISynchronizable))]
+    sealed class ThreadingTest: SemanticTestBase
+    {
+        [Test(Description="Fork/await test.")]
+        public void ForkAwaitTest()
+        {
+            var r = Run("var a = fork {threading.sleep(1000); leave 2;}; return await a while threading.sleep(2000);");
+            Assert.IsTrue(r);
+        }
+    }
+}
