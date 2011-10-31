7c51063cbf55947ec1511d8541200addb2c8f4de
Roman Sakno <sakno@tncor.com>
2011-10-31 12:48:53 +1100

    Module for integration with .NET Classes

---
+++ csharp/src/modules/ClrTypes/Module.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DynamicScript.Runtime;
+using DynamicScript.Runtime.Environment;
+
+namespace DynamicScript.Modules.ClrTypes
+{
+    using ComVisibleAttribute = System.Runtime.InteropServices.ComVisibleAttribute;
+
+    /// <summary>
+    /// Represents implementation of ClrTypes module.
+    /// This class cannot be inherited.
+    /// </summary>
+    [ComVisible(false)]
+    public sealed class Module: ScriptCompositeObject
+    {
+        /// <summary>
+        /// Executes an entry point of the module.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public static IScriptObject Run(InterpreterState state)
+        {
+            return new Module();
+        }
+    }
+}
