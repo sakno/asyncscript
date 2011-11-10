71f846442c3983a7a261c2e2c251a7aec31c1045
sakno <sakno@tncor.com>
2011-11-10 22:04:39 +1100

    Beta 0.8

--- csharp/src/interpreter/Properties/AssemblyInfo.cs
+++ csharp/src/interpreter/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using PEFileKinds = System.Reflection.Emit.PEFileKinds;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.*")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7 beta")]
+[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyInformationalVersion("0.8 beta")]
--- csharp/src/modules/ClrTypes/Properties/AssemblyInfo.cs
+++ csharp/src/modules/ClrTypes/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using ClrTypes = DynamicScript.Modules.ClrTypes.Module;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.*")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7 beta")]
+[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyInformationalVersion("0.8 beta")]
