c710586e2156b7745e9bbcf0f3d28edd6596fa3e
sakno <sakno@tncor.com>
2011-11-11 00:35:14 +1100

    Console-detached host for DynamicScript programs

---
+++ csharp/src/host/EmptyTextReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace DynamicScript.Runtime.Hosting
+{
+    using ScriptObject = Runtime.Environment.ScriptObject;
+
+    sealed class EmptyTextReader: DynamicScriptInput
+    {
+
+        public override void ReadLine(out IScriptObject obj)
+        {
+            obj = ScriptObject.Void;
+        }
+    }
+}
---
+++ csharp/src/host/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DynamicScript.Runtime.Hosting
+{
+    using ComVisibleAttribute = System.Runtime.InteropServices.ComVisibleAttribute;
+    using Encoding = System.Text.Encoding;
+
+    [ComVisible(false)]
+    sealed class Program
+    {
+
+        private static int Execute(CommandLineParser cmd, string[] args)
+        {
+            return cmd.Run(args);
+        }
+
+        [LoaderOptimization(LoaderOptimization.SingleDomain)]
+        private static int Main(string[] args)
+        {
+            using (var output = new FileStream("output.log", FileMode.Append, FileAccess.Write, FileShare.Read))
+            using (var writer = new StreamWriter(output, Encoding.UTF8))
+            using (var reader = new EmptyTextReader())
+                return Execute(new CommandLineParser(writer, reader), args);
+        }
+    }
+}
