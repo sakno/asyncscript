2f0207017481e61df0fe7d5e3de3d236bd9ce063
sakno <sakno@tncor.com>
2011-11-17 20:08:01 +1100

    Beta 0.8.1

--- csharp/src/interpreter/Properties/AssemblyInfo.cs
+++ csharp/src/interpreter/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using PEFileKinds = System.Reflection.Emit.PEFileKinds;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8 beta")]
+[assembly: AssemblyVersion("0.8.1.*")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyInformationalVersion("0.8.1 beta")]
